97065401ca7712afdf82fa2de2d0eb3efa594020
Dylain Cohen <dylain.cohen@wework.com>
2018-02-18 23:15:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,7 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      true
       did_win = true
       return win_combination # return the win_combination indexes that won.
     else
